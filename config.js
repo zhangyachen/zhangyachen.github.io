fea070887444f826467425dec6979967fe110d4d
zhangyachen01 <zhangyachen@baidu.com>
2015-09-03 21:15:44 +0800

    blob

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _config = {
     owner           : 'zhangyachen01',           // github 用户名
     repo            : 'zhangyachen01.github.io',// github 中对应项目名
     duoshuo_id      : 'zhangyachen01',            // 在第三方评论插件多说申请站点的子域名
-    access_token : 'cab8619e0737967f3679862eb7efdff00688db50',        // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    access_token : '8eb3999fe86355c670f4d9095265fea1ef3c3854',        // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page        : '20'                    // 默认一页显示几篇文章
 }
 
